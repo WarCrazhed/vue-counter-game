c1ddc481842cfa85ebb018bb87bf560310881643
Mario Zamora <zamora@Mac-mini-de-Mario-5.local>
2025-09-12 09:20:28 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import GameCounter from '@/components/GameCounter.vue';
+</script>
+
+<template>
+  <h1>Hello World!</h1>
+  <GameCounter />
+</template>
+
+<style scoped></style>
---
+++ src/components/GameCounter.vue
@@ -0,0 +1,16 @@
+<script setup>
+  console.log('GameCounter component loaded');
+
+</script>
+
+<template>
+  <button>menos</button>
+  <span>Contador: 0</span>
+  <button>más</button>
+</template>
+
+<style>
+  button {
+    background-color: red; /* Green */
+  }
+</style>
