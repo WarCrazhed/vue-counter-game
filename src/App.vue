b2c7788ac39ef506026251e5acaff72e959d59a4
Mario Zamora <zamora@Mac-mini-de-Mario-5.local>
2025-09-19 09:00:59 -0600

    Reactivity vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
-import GameCounter from '@/components/GameCounter.vue';
+import GameCounter from '@/components/GameCounter.vue'
+
+const MIN_NUMBER = 0
+const MAX_NUMBER = 50
 </script>
 
 <template>
-  <h1>Hello World!</h1>
-  <GameCounter />
+  <div
+    class="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-900 via-black to-slate-900"
+  >
+    <GameCounter :min-number="MIN_NUMBER" :max-number="MAX_NUMBER" />
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/GameCounter.vue
+++ src/components/GameCounter.vue
@@ -1,24 +1,58 @@
 <script setup>
-  const isActive = false;
+import { ref } from 'vue'
+
+const counter = ref(0)
+
+const increment = () => {
+  counter.value++
+}
+
+const decrement = () => {
+  counter.value--
+}
+
+const { minNumber, maxNumber } = defineProps({
+  minNumber: {
+    type: Number,
+    required: true,
+  },
+  maxNumber: {
+    type: Number,
+    required: true,
+  },
+
+  example: {
+    type: Object,
+    default: () => ({
+      name: 'Mario',
+    }),
+  },
+})
+
+console.log(minNumber, maxNumber)
 </script>
 
 <template>
-  <button>menos</button>
-  <span class="counter" :class="{ 'counter--active': isActive }" style="font-size: 4rem;">Contador: 0</span>
-  <button>más</button>
+  <div
+    class="text-center flex flex-col items-center gap-5 border border-slate-800 w-2xl mx-5 max-w-6xl p-10 rounded-3xl bg-black/50 backdrop-blur-sm text-white shadow-2xl shadow-indigo-950"
+  >
+    <p class="text-2xl font-bold mt-5">Guess the number!</p>
+    <p class="text-5xl font-bold hover:scale-115 duration-200 ease-in-out mb-10">{{ counter }}</p>
+    <div class="flex gap-2 justify-center">
+      <button
+        class="bg-red-500 hover:bg-red-600 text-white font-extrabold py-2 px-4 rounded-xl text-2xl"
+        @click="decrement"
+      >
+        -
+      </button>
+      <button
+        class="bg-lime-500 hover:bg-lime-600 text-white font-extrabold py-2 px-4 rounded-xl text-2xl"
+        @click="increment"
+      >
+        +
+      </button>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-  .counter {
-    font-weight: bold;
-    font-size: 2rem;
-  }
-
-  .counter--active {
-    color: aqua;
-  }
-
-  button {
-    background-color: red; /* Green */
-  }
-</style>
+<style scoped></style>
